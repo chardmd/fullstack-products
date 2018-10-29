972033b3e2e7beef16c1488c35c6227070eaac88
Richard <richard.dimalanta@gmail.com>
2018-10-29 20:22:12 +1100

    updated test for pagination

--- src/components/Paginate/test.js
+++ src/components/Paginate/test.js
@@ -3,15 +3,15 @@ import { shallow } from "enzyme";
 import Paginate from "./index";
 
 describe("<Paginate />", () => {
-  it("< Previous page button should be disabled", () => {
+  it("Should have an active page", () => {
     const wrapper = shallow(
       <Paginate
-        activePage={1}
+        activePage={7}
         itemsCountPerPage={10}
         totalItemsCount={10}
         onGetData={() => {}}
       />
     );
-    expect(wrapper.find(".disabled")).not.toBeUndefined();
+    expect(wrapper.find(".active")).not.toBeUndefined();
   });
 });
